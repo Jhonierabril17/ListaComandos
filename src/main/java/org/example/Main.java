c9eb813f102dcc2ac0d4a9694cd5d2a405a95e7c
Jhonier_Abril <73132677+Jhonierabril17@users.noreply.github.com>
2025-02-18 19:59:19 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!\n");
-        System.out.printf("proyecto nuevo");
-        System.out.printf("Nuevo cambio");
+        System.out.printf("proyecto nuevo \n");
+        System.out.printf("Nuevo cambio \n");
+        System.out.printf("Cambio desde Git\n");
     }
 }
