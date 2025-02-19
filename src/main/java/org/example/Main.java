58a8483c763d33dd862f5bdcce347c1ecdacf4fd
jdabril <abril.2117jhonier@gmail.com>
2025-02-18 20:37:39 -0500

    Guardando cambios antes de cambiar de rama

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!\n");
-        System.out.printf("proyecto nuevo \n");
-        System.out.printf("Nuevo cambio \n");
-        System.out.printf("Cambio desde Git\n");
+        System.out.printf("proyecto nuevo");
+        System.out.printf("Nuevo cambio");
     }
 }
