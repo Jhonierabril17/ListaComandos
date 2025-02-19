2906d4ca8eb0d694682d85c1a35d8224295cac51
jdabril <abril.2117jhonier@gmail.com>
2025-02-19 16:49:19 -0500

    CambiosRamaPrueba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.printf("Nuevo cambio 3");
         System.out.printf("Nuevo cambio 4");
         System.out.printf("Nuevo cambio 5");
+        System.out.printf("Nuevo cambio 6");
     }
 }
